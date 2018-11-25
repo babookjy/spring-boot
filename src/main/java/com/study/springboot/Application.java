8450fdb23e8b5e0e48eacbdf13e2850d649a3973
jaeyoung <jaeyoung@jaeyoung-PC>
2018-11-25 22:52:48 +0900

    pom

--- src/main/java/com/study/springboot/Application.java
+++ src/main/java/com/study/springboot/Application.java
@@ -2,6 +2,7 @@ package com.study.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,12 +17,12 @@ import lombok.extern.slf4j.Slf4j;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ApplicationContext context = SpringApplication.run(Application.class, args);
 		
-		/*String[] beanNames = context.getBeanDefinitionNames();
+		String[] beanNames = context.getBeanDefinitionNames();
 		
 		for (String bean : beanNames) {
 			log.info(bean);
-		}*/
+		}
 	}
 }
