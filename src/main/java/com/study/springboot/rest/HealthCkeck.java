9935314c29484ba1310136137b31e02cb98f9569
jaeyoung <jaeyoung@jaeyoung-PC>
2019-02-19 22:33:04 +0900

    jenkins 연동 테스트

---
+++ src/main/java/com/study/springboot/rest/HealthCkeck.java
@@ -0,0 +1,13 @@
+package com.study.springboot.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCkeck {
+
+	@GetMapping("/health")
+	public String getHealth() {
+		return "ok";
+	}
+}
